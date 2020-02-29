ae107d4f2e9226676edac59224043fb3a98580fa
Noman Rafiq <59981447+imnomee@users.noreply.github.com>
2020-02-29 17:58:05 +0000

    update and working yay

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { recipes } = require('../data/data.json');
+const { recipes } = require("../data/data.json");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  // 1. Pass all recipe data to 'index' template
-  res.render('index');
+router.get("/", function(req, res, next) {
+    res.render("index", { recipes });
 });
 
 /* GET recipe page. */
-router.get('/recipes/:id', function(req, res, next) {
-  const recipeId = req.params.id;
-  const recipe = recipes.find( ({ id }) => id === +recipeId );
-  
-  if (recipe) {
-    // 2. Pass the recipe data to the 'recipe' template
-    res.render('recipe');
-  } else {
-    res.sendStatus(404);
-  }
+router.get("/recipes/:id", function(req, res, next) {
+    const recipeId = req.params.id;
+    const recipe = recipes.find(({ id }) => id === +recipeId);
+
+    if (recipe) {
+        res.render("recipe", { recipe });
+    } else {
+        res.sendStatus(404);
+    }
 });
 
 module.exports = router;

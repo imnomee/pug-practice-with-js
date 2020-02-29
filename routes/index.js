ccaf574933ce6dbc3631b798198b3df0e7889657
Noman Rafiq <59981447+imnomee@users.noreply.github.com>
2020-02-29 15:11:23 +0000

    final

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const { list } = require("../data/data.json");
 
 /* GET home page. */
 router.get("/", function(req, res) {
-    // 1. Pass all recipe data to 'index' template
     res.render("index", { list });
 });
 
@@ -12,9 +11,7 @@ router.get("/", function(req, res) {
 router.get("/recipes/:id", function(req, res) {
     const recipeId = req.params.id;
     const recipe = list.find(({ id }) => id === +recipeId);
-
     if (recipe) {
-        // 2. Pass the recipe data to the 'recipe' template
         res.render("recipe", { recipe });
     } else {
         res.sendStatus(404);
